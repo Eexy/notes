046eb1b40919d66d88b761963e8dd9009bed1958
eexy <eexhde@gmail.com>
2020-10-07 13:57:45 +0200

    can read the note file

--- utils.js
+++ utils.js
@@ -1,9 +1,23 @@
 const fs = require("fs");
 
 const createNote = (title, body) => {
-	const note = { title, body };
+    const note = { title, body };
+    const notes = getNotes();
 
-	fs.writeFileSync("notes.json", JSON.stringify(note));
+    notes.push(note);
+
+	fs.writeFileSync("notes.json", JSON.stringify(notes));
 };
+
+const getNotes = () => {
+    if (!fs.existsSync("notes.json")) {
+        return [];
+    }
+
+    const buffer = fs.readFileSync("notes.json");
+    const data = JSON.parse(buffer.toString());
+
+    return data;
+}
 
 module.exports.createNote = createNote;
